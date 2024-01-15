34c3debbb207f6de99a4c2edab201b22fca9c3b1
bonihachi <tmurono@outlook.com>
2024-01-15 16:49:18 +0900

    added smoothcursor.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -126,6 +126,8 @@ return packer.startup(function(use)
 
   use({ 'lewis6991/impatient.nvim' })
 
+  use ({ 'gen740/SmoothCursor.nvim' })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["SmoothCursor.nvim"] = {
+    loaded = true,
+    path = "/home/bonihachi/.local/share/nvim/site/pack/packer/start/SmoothCursor.nvim",
+    url = "https://github.com/gen740/SmoothCursor.nvim"
+  },
   ["alpha-nvim"] = {
     config = { "\27LJ\2\na\0\0\5\0\5\0\n6\0\0\0'\2\1\0B\0\2\0029\0\2\0006\2\0\0'\4\3\0B\2\2\0029\2\4\2B\0\2\1K\0\1\0\vconfig\27alpha.themes.dashboard\nsetup\nalpha\frequire\0" },
     loaded = true,

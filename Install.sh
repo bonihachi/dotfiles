34fd0136641c313e3172b1bfe00df81bd4b59629
bonihachi <taiki_murono@iwi.co.jp>
2024-04-21 15:57:01 +0900

    create symlink for vscode keybindings

--- Install.sh
+++ Install.sh
@@ -13,3 +13,4 @@ ln -fns ${SCRIPT_DIR_PATH}/.config/nvim ~/.config/nvim
 ln -fns ${SCRIPT_DIR_PATH}/.config/fish ~/.config/fish
 ln -fns ${SCRIPT_DIR_PATH}/.config/alacritty ~/.config/alacritty
 ln -fns ${SCRIPT_DIR_PATH}/.tmux.conf ~/.tmux.conf
+ln -fns ${SCRIPT_DIR_PATH}/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
--- Uninstall.sh
+++ Uninstall.sh
@@ -11,3 +11,4 @@ rm ~/.config/nvim
 rm ~/.config/fish
 rm ~/.config/alacritty
 rm ~/.tmux.conf
+rm ~/.config/Code/User/keybindings.json

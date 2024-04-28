45a5055d9efd259bbbe0ad241fd4dc214ec2b186
bonihachi <bonihachi@gmail.com>
2024-04-28 11:22:01 +0900

    modified cmp settings

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -25,7 +25,6 @@ return {
             diagnostics = "nvim_lsp",
             always_show_bufferline = true,
             diagnostics_indicator = function(_, _, diag)
-            local icons = require("lazyvim.config").icons.diagnostics
             local ret = (diag.error and icons.Error .. diag.error .. " " or "")
                 .. (diag.warning and icons.Warn .. diag.warning or "")
             return vim.trim(ret)

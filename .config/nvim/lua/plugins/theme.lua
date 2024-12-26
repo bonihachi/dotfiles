717c353eb522af8d8058781f36d62363e5b349cf
bonihachi <taiki_murono@iwi.co.jp>
2024-12-26 16:34:37 +0900

    monochrome

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,16 +1,12 @@
 return {
 	--'cocopon/iceberg.vim',
-	--"kyazdani42/blue-moon",
-	-- "rockerBOO/boo-colorscheme-nvim",
 	-- "rebelot/kanagawa.nvim",
-	-- "jim-at-jibba/ariake.nvim",
-	-- "mhartington/oceanic-next",
 	-- "tiagovla/tokyodark.nvim",
-	-- "mhartington/oceanic-next",
-	"EdenEast/nightfox.nvim",
+	-- "EdenEast/nightfox.nvim",
+	"kdheepak/monochrome.nvim",
 	enabled = true,
 	event = "VeryLazy",
 	config = function()
-		vim.cmd.colorscheme("nightfox")
+		vim.cmd.colorscheme("monochrome")
 	end,
 }
